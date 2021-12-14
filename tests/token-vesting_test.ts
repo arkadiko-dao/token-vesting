3d6542e6458505e9ef0ec4bad2b66a2148786d1d
Chris Guimaraes <cguimaraes.br@gmail.com>
2021-12-14 20:43:08 +0000

    Add initial contract test

--- tests/token-vesting_test.ts
+++ tests/token-vesting_test.ts
@@ -1,26 +1,65 @@
+import {
+    Clarinet,
+    Tx,
+    Chain,
+    Account,
+    types
+} from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
 
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
-import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+import {
+    assertEquals
+} from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+const addr = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
+const contract = "token-vesting";
+
+/**
+ * Bridge to test token
+ */
+class TokenBridge {
+
+    chain: Chain;
+    deployer: Account;
+
+    constructor(chain: Chain, deployer: Account) {
+        this.chain = chain;
+        this.deployer = deployer;
+    }
+
+    /**
+     * Get qualified token contract
+     */
+    getTokenContract(token: string) {
+        return `${addr}.${token}`;
+    }
+
+    /**
+     * Deposit function
+     */
+    deposit(token:string, amount: number) {
+        const block = this.chain.mineBlock([
+			      Tx.contractCall(
+                contract,
+                'deposit',
+                [
+                    types.principal(this.getTokenContract(token)),
+                    types.uint(amount)
+                ],
+                this.deployer.address
+            )
+		    ]);
+
+        return block.receipts[0].result;
+    }
+}
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "allows a deposit",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+        const amount = 100;
+        const token = new TokenBridge(chain, accounts.get("deployer")!);
+        const result = token.deposit("xyz-token", amount);
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
-    },
+        result.expectOk().expectBool(true);
+    }
 });

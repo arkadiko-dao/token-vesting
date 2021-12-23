c019a09f05e69f5688a04391a499e9acce17c6ea
Chris Guimaraes <cguimaraes.br@gmail.com>
2021-12-23 16:22:13 +0000

    updating tests

--- tests/token-vesting_test.ts
+++ tests/token-vesting_test.ts
@@ -10,14 +10,14 @@ import {
     assertEquals
 } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
-const addr = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
+const addr = "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM";
 const contract = "token-vesting";
+const token = "xyz-token";
 
 /**
  * Bridge to test token
  */
-class TokenBridge {
-
+class ContractBridge {
     chain: Chain;
     deployer: Account;
 
@@ -27,39 +27,68 @@ class TokenBridge {
     }
 
     /**
-     * Get qualified token contract
+     * Get qualified token address
      */
-    getTokenContract(token: string) {
+    getTokenAddress() {
         return `${addr}.${token}`;
+    }
+
+    /**
+     * Get qualified contract address
+     */
+    getAddress() {
+        return `${addr}.${contract}`;
     }
 
     /**
      * Deposit function
      */
-    deposit(token:string, amount: number) {
+    deposit(token: string, amount: number, lockingPeriod: number, assignees: { address: string, amount: number }[]) {
+        let assigneesList: any[] = [];
+        assignees.forEach((el) => {
+            assigneesList.push(
+                types.tuple({
+                    'address': types.principal(el.address),
+                    'amount': types.uint(el.amount)
+                })
+            )
+        });
+
         const block = this.chain.mineBlock([
 			      Tx.contractCall(
                 contract,
                 'deposit',
                 [
-                    types.principal(this.getTokenContract(token)),
-                    types.uint(amount)
+                    types.principal(this.getTokenAddress()),
+                    types.uint(amount),
+                    types.uint(lockingPeriod),
+                    types.list(assigneesList)
                 ],
                 this.deployer.address
             )
-		    ]);
+	      ]);
 
-        return block.receipts[0].result;
+        return block.receipts[0];
     }
 }
 
 Clarinet.test({
-    name: "allows a deposit",
+    name: "[deposit] amount to be locked is transferred to the contract",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        const amount = 100;
-        const token = new TokenBridge(chain, accounts.get("deployer")!);
-        const result = token.deposit("xyz-token", amount);
+        const deployer = accounts.get("deployer")!;
+        const amount = 1020;
+        const lockingPeriod = 48;
+        const assignees = [{
+            address: accounts.get("wallet_3")!.address,
+            amount: 25
+        }];
 
-        result.expectOk().expectBool(true);
+        const contract = new ContractBridge(chain, deployer);
+        const resp = contract.deposit(token, amount, lockingPeriod, assignees);
+        const [transferEvent, _] = resp.events
+
+        resp.result.expectOk().expectBool(true);
+        transferEvent.ft_transfer_event.amount.expectInt(amount);
+        transferEvent.ft_transfer_event.recipient.expectPrincipal(contract.getAddress());
     }
 });
